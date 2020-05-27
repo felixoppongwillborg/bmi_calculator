944cb79a6c52b337708cea25e368e639768424ec
felixoppongwillborg <felixwillborg@MacBook-Pro-som-tillhor-Felix.local>
2020-05-27 11:12:40 +0200

    added calculate button, updated all srcfiles

--- cypress/integration/userCanChechTheirBmi.feature.js
+++ cypress/integration/userCanChechTheirBmi.feature.js
@@ -4,5 +4,10 @@ describe('User can check their Bmi', () => {
         cy.contains('BMI Calculator')
         cy.get('#weight').type ('90')
         cy.get('#height').type ('186')
+        cy.get('#Calculate').click()
         })
+
+    it('Displays a BMI value of 26.01', () =>{
+        cy.get('#results').should ('contain', '26.01')
+    })   
 })
