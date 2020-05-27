62b09d33c4c67aa12b158bfc2f7f610f8fcb244d
felixoppongwillborg <felixwillborg@MacBook-Pro-som-tillhor-Felix.local>
2020-05-27 13:13:27 +0200

    calculator is ready but without any styling

--- src/assets/js/bmiCalculator.js
+++ src/assets/js/bmiCalculator.js
@@ -2,5 +2,14 @@ const calculateMetricBMI = (weight, height) => {
     let result
     result = weight / ((height / 100) * (height / 100))
     return result = (weight / ((height / 100) * (height / 100))).toFixed(2)
+}
+const getBMIclassification = value => {
+    if (value < 18.5){
+        return "underweight"
+    } else if (value < 25){
+        return "Healty"
+    } else if (value < 30){  
+        return "Overweight"     
+    }
 
 }
