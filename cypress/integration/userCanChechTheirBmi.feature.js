8d817ea37e752df281242d22aab670977b29fb82
felixoppongwillborg <felixwillborg@MacBook-Pro-som-tillhor-Felix.local>
2020-06-01 09:56:27 +0200

    took away src before --port 3000 in json file, now all tests work

--- cypress/integration/userCanChechTheirBmi.feature.js
+++ cypress/integration/userCanChechTheirBmi.feature.js
@@ -1,6 +1,6 @@
 describe('User can check their Bmi', () => {
     it('User can enter weight and height', () => {
-        cy.visit('http://localhost:3001')
+        cy.visit('http://localhost:3000')
         cy.contains('BMI Calculator')
         cy.get('#weight').type ('90')
         cy.get('#height').type ('186')
